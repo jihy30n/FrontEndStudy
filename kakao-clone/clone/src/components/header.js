19526a38dd6d06f4796adf78ab482a1af7200c89
jihy30n <jihyeon546@gmail.com>
2023-11-07 15:36:27 +0900

    토글버튼

--- kakao-clone/clone/src/components/header.js
+++ kakao-clone/clone/src/components/header.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAdjust, faLanguage } from '@fortawesome/free-solid-svg-icons';
 
 const HeaderContainer = styled.div`
    display: flex;
@@ -19,10 +21,39 @@ const NavItem = styled.a`
    text-decoration: none;
 `;
 
+const togleButtons = styled.div`
+    margin: 50px;
+
+
+`
+
+const togleButton = styled.button`
+
+  background-color: black;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  cursor: pointer;
+
+`
+
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const [langugeMode, setLanguageMode] = useState(false);
+
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const toggleLangugeMode = () => {
+    setLanguageMode(!langugeMode);
+
+  };
+
   return (
     <HeaderContainer>
-      <Logo>로고</Logo>
+      <Logo><img src="https://picsum.photos/74/24" alt="logo"/></Logo>
       <NavItems>
         <NavItem href="#">소개</NavItem>
         <NavItem href="#">이야기</NavItem>
@@ -30,6 +61,16 @@ const Header = () => {
         <NavItem href="#">기술과 서비스</NavItem>
         <NavItem href="#">약속과 책임</NavItem>
       </NavItems>
+      <togleButtons>
+        <togleButton onClick={toggleLangugeMode}>
+          {langugeMode ? "korean":"English"}
+        {/* <FontAwesomeIcon icon={Language} /> */}
+      </togleButton>
+      <togleButton onClick={toggleDarkMode}>
+        {darkMode ? "라이트 모드" : "다크 모드"}
+      </togleButton>
+      </togleButtons>
+     
     </HeaderContainer>
   );
 }
